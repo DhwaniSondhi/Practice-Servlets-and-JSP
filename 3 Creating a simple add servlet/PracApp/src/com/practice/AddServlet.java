6b310eeff2fa647c1ec2b82798353bb2c268ff7a
DSondhi <dhwanisondhi3@gmail.com>
2020-04-28 07:32:42 -0400

    Adding Lectures 1, 2 and 3

---
+++ 3 Creating a simple add servlet/PracApp/src/com/practice/AddServlet.java
@@ -0,0 +1,23 @@
+package com.practice;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class AddServlet extends HttpServlet{
+
+	public void service(HttpServletRequest request, HttpServletResponse response) {
+		int num1=Integer.parseInt(request.getParameter("num1"));
+		int num2=Integer.parseInt(request.getParameter("num2"));
+		int sum=num1+num2;
+		try {
+			PrintWriter wrt=response.getWriter();
+			wrt.println("result:"+sum);
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+}
